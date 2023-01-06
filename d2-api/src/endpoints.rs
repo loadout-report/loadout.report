800358fd88dc3276af002a168ca40e8754b5a100
Carlo Field <cfi@bluesky-it.ch>
2023-01-06 01:56:50 +0100

    manifest endpoint :)

--- d2-api/src/endpoints.rs
+++ d2-api/src/endpoints.rs
@@ -1,5 +1,12 @@
+use thiserror::Error;
 
 pub mod auth;
 pub mod destiny;
 
-
+#[derive(Error, Debug)]
+pub enum ApiError {
+    #[error("Error in request: {0}")]
+    ReqwestError(reqwest::Error),
+    #[error("Error parsing JSON: {0}")]
+    JsonParseError(reqwest::Error),
+}
--- d2-api/src/endpoints/destiny.rs
+++ d2-api/src/endpoints/destiny.rs
@@ -1,3 +1,18 @@
+use thiserror::Error;
+use crate::destiny::config::Manifest;
 use crate::destiny::Hash;
+use crate::endpoints::ApiError;
 
+#[derive(Error, Debug)]
+pub enum DestinyEndpointError {
 
+}
+
+pub async fn get_destiny_manifest(client: &reqwest::Client) -> Result<Manifest, ApiError> {
+    client.get("https://www.bungie.net/Platform/Destiny2/Manifest/")
+        .send().await
+        .map_err(ApiError::ReqwestError)?
+        .json()
+        .await
+        .map_err(ApiError::JsonParseError)
+}
