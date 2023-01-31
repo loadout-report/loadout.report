c6e338a4c862ab4a92361f48212744245bf3f631
Carlo Field <carlo@swiss.dev>
2023-01-31 17:29:38 +0000

    update crawler

--- crawler/src/bin/crawler.rs
+++ crawler/src/bin/crawler.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 use anyhow::Context;
 use ipnet::IpNet;
 use reqwest::Client;
+use log::info;
 
 #[cfg(target_os = "windows")]
 #[global_allocator]
@@ -14,16 +15,19 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::init();
     let range = args().nth(1).context("Missing IP range")?;
     let range = IpNet::from_str(&range).context("Invalid IP range")?;
     let host = range.hosts().next().context("IP range exhausted")?;
 
+    info!("creating client");
     let client: Client = reqwest::ClientBuilder::new()
-        .local_address(host)
+        .local_address(host.clone())
         .freebind(true)
         .build()
         .unwrap();
 
+    info!("sending request to {:?}", host);
     let text = client.get("https://am.i.mullvad.net").send()
         .await?
         .text()
